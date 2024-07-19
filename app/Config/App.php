86b7cc8f067c4c54c487ff2a6d630f2cd803f21d
marcelaalegre <marcelita.alegre23@gmail.com>
2024-07-19 14:17:19 -0300

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/primer_proyecto';
+    public string $baseURL = 'http://localhost/ProT3_36317556';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
